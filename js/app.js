83c3e5f05b0403fc8270218b29ac51de73cd7642
moskvin73 <moskvin73av@gmail.com>
2026-07-07 00:18:07 +0300

    1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //import { texToMathML, texToOMML } from './parser.js?v=18';
-import { texToMathML, texToOMML } from './parser2.js?v=22';
+import { texToMathML, texToOMML } from './parser2.js?v=23';
 
 let currentOMML = "";
 
