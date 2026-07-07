53d9bf6c8932e5218dcfa0774fe8949dde2fd221
moskvin73 <moskvin73av@gmail.com>
2026-07-07 22:50:04 +0300

    1

--- js/parser2.js
+++ js/parser2.js
@@ -1083,7 +1083,7 @@ function renderMathML(nodes) {
             let attributes = '';
             
             if (node.subType === 'skewed') {
-              return `<mrow><mrow style="math-depth: add(1); vertical-align: 0.4em;">${wrapInMrowIfNeeded(node.num)}</mrow><mo style="padding: 0 0.1em; vertical-align: 0.1em;">&#x2044;</mo><mrow style="math-depth: add(1); vertical-align: -0.2em;">${wrapInMrowIfNeeded(node.den)}</mrow></mrow>`
+              return `<mrow><mrow style="display: inline-block; vertical-align: 0.35em; math-depth: add(1);">${wrapInMrowIfNeeded(node.num)}</mrow><mo style="display: inline-block; padding: 0 0.05em; vertical-align: 0.05em;">&#x2044;</mo><mrow style="display: inline-block; vertical-align: -0.15em; math-depth: add(1);">${wrapInMrowIfNeeded(node.den)}</mrow></mrow>`
                 //attributes = ' bevelled="true"'; // Косая диагональная дробь
             } else if (node.subType === 'small') {
                 attributes = ' scriptlevel="1" DISPLAYSTYLE="false"'; // Принудительно уменьшает \tfrac в MathML Core
